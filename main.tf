eb5dc7c554ea107ec4ae664b26bacb79b1e6b232
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-17 22:31:09 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_network_interface" "example" {
+  subnet_id = aws_subnet.example.id
+  private_ips = ["10.0.0.10"]
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id = aws_network_interface.example.id
+    device_index         = 0
+  }
+}
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+  vpc_id      = aws_vpc.example.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
