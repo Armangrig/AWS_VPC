4c17f25b1893304c818ddd69ba36b0fd3d665bfc
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-21 13:20:27 +0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -14,3 +14,24 @@ terraform {
     region = "us-east-1"
   }
 }
+
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
