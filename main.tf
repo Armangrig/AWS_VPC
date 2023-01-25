7d8826cd6a18bb9648237aab2eab9990c1feeced
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-25 22:03:21 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,13 +16,13 @@ resource "aws_s3_bucket" "tf_state" {
   acl    = "private"
 }
 
-#terraform {
-#  backend "s3" {
-#    bucket = "lennakanhay"
-#    key    = "state.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "lennakanhay"
+    key    = "state.tfstate"
+    region = "us-east-1"
+  }
+}
 
 
 
