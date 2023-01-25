ed975e0d6e055bdb85a325fa7267f9610c62fc44
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-26 00:08:56 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-data "http" "website" {
-  url = "http://${aws_instance.web.public_ip}"
-}
+#data "http" "website" {
+#  url = "http://${aws_instance.web.public_ip}"
+#}
 
 #output "curl_response" {
 #  value = data.http.website.body
