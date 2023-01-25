c28aa6c11963e603fb421ea4c756fcbc14d917db
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-25 23:47:44 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,12 +6,4 @@ output "curl_response" {
   value = data.http.website.body
 }
 
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
-}
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
