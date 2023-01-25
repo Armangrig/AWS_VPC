7bffd4202f7fcb28e58e16cfdbbf4dbed821cf90
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-25 23:32:40 +0400

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+myip=`curl http://169.254.169.253/latest/meta-data/local-ipv4`
  
 cat <<EOF > /var/www/html/index.html
 <html>
 <h2>Build by Power of Terraform <font color="red">/font></h2><br>
-Owner Artur Ayvazyan
-Hello from artur
+Owner Grigoryan
+Hello from arman
 </html>
 EOF
 
