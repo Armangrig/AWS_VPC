47d802891664fbdddecc840131ab88793e1ec793
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-28 15:47:47 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "gugushik"
+#  variable "instance_name" {
+#  description = "Value of the Name tag for the EC2 instance"
+#  type        = string
+#  default     = "gugushik"
+#}
+
+
+variable "task_name" {
+  type    = string
+  default = "s3_to_tfcloud"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
 }
