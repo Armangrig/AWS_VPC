d57abcfccfbf03ab4aec37addeaeb9c207eeae4d
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-28 15:54:33 +0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,6 @@
-#data "http" "website" {
-#  url = "http://${aws_instance.web.public_ip}"
-#}
-
-#output "curl_response" {
-#  value = data.http.website.body
-#}
-
-#output "instance_id" {
-#  description = "ID of the EC2 instance"
-#  value       = aws_instance.app_server.id
-#}
-
-#output "instance_public_ip" {
-#  description = "Public IP address of the EC2 instance"
-#  value       = aws_instance.app_server.public_ip
-#}
+output "first_instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.first_aws_instance.public_ip
+}
 
 
